fbcdf062ed41036a2f825a00f341ce697af9093f
Mateo <jumninom@udistrital.edu.co>
2024-10-17 15:49:45 -0500

    se agraga el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace VinoTanquesForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNombreVino = new System.Windows.Forms.TextBox();
+            this.textBoxRadio = new System.Windows.Forms.TextBox();
+            this.textBoxAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.comboBoxMaterial = new System.Windows.Forms.ComboBox();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del Vino";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Radio (m)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Altura";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Material";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(53, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Resultado";
+            // 
+            // textBoxNombreVino
+            // 
+            this.textBoxNombreVino.Location = new System.Drawing.Point(214, 42);
+            this.textBoxNombreVino.Name = "textBoxNombreVino";
+            this.textBoxNombreVino.Size = new System.Drawing.Size(121, 22);
+            this.textBoxNombreVino.TabIndex = 6;
+            // 
+            // textBoxRadio
+            // 
+            this.textBoxRadio.Location = new System.Drawing.Point(214, 80);
+            this.textBoxRadio.Name = "textBoxRadio";
+            this.textBoxRadio.Size = new System.Drawing.Size(121, 22);
+            this.textBoxRadio.TabIndex = 7;
+            // 
+            // textBoxAltura
+            // 
+            this.textBoxAltura.Location = new System.Drawing.Point(214, 115);
+            this.textBoxAltura.Name = "textBoxAltura";
+            this.textBoxAltura.Size = new System.Drawing.Size(121, 22);
+            this.textBoxAltura.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(56, 201);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(279, 23);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxMaterial
+            // 
+            this.comboBoxMaterial.FormattingEnabled = true;
+            this.comboBoxMaterial.Items.AddRange(new object[] {
+            "-",
+            "Madera",
+            "Metal"});
+            this.comboBoxMaterial.Location = new System.Drawing.Point(214, 154);
+            this.comboBoxMaterial.Name = "comboBoxMaterial";
+            this.comboBoxMaterial.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMaterial.TabIndex = 11;
+            this.comboBoxMaterial.Text = "-";
+            this.comboBoxMaterial.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(53, 259);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(0, 16);
+            this.labelResultado.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 318);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.comboBoxMaterial);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.textBoxAltura);
+            this.Controls.Add(this.textBoxRadio);
+            this.Controls.Add(this.textBoxNombreVino);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VinoTanque";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxNombreVino;
+        private System.Windows.Forms.TextBox textBoxRadio;
+        private System.Windows.Forms.TextBox textBoxAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox comboBoxMaterial;
+        private System.Windows.Forms.Label labelResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VinoTanquesForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evento del botón Calcular
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Captura los valores ingresados
+                string nombreVino = textBoxNombreVino.Text;
+                double radio = Convert.ToDouble(textBoxRadio.Text);
+                double altura = Convert.ToDouble(textBoxAltura.Text);
+                string material = comboBoxMaterial.SelectedItem?.ToString();
+
+                // Validación de los datos ingresados
+                if (string.IsNullOrWhiteSpace(nombreVino))
+                {
+                    MessageBox.Show("Por favor, ingresa el nombre del vino.", "Entrada Inválida");
+                    return;
+                }
+                if (radio <= 0 || altura <= 0)
+                {
+                    MessageBox.Show("Radio y altura deben ser mayores que 0.", "Entrada Inválida");
+                    return;
+                }
+                if (material == "-")
+                {
+                    MessageBox.Show("Por favor, selecciona un material válido.", "Entrada Inválida");
+                    return;
+                }
+
+                // Calcula el volumen del tanque (en metros cúbicos)
+                double volumenMetrosCubicos = Math.PI * Math.Pow(radio, 2) * altura;
+                double volumenLitros = volumenMetrosCubicos * 1000; // Conversión a litros
+
+                // Muestra el resultado en el Label
+                labelResultado.Text = $"Hay {volumenLitros:F2} litros del {nombreVino} vino en un depósito de {material}.";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa valores numéricos válidos para el radio y la altura.", "Error de Formato");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+        }
+
+        // Eventos adicionales para evitar errores
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+    }
+}
+
